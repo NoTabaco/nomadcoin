f9e91a7ae9e712578362061118b95cc6535eb303
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-09-21 22:28:02 +0900

    Using Partials

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ import (
 	"github.com/NoTabaco/nomadcoin/blockchain"
 )
 
-const port string = ":4000"
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
+
+var templates *template.Template
 
 type homeData struct {
 	PageTitle string
@@ -17,12 +22,15 @@ type homeData struct {
 }
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseGlob("templates/pages/home.gohtml"))
 	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	tmpl.Execute(rw, data)
+	templates.ExecuteTemplate(rw, "home", data)
 }
 
 func main() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	fmt.Println(templates.Tree)
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+	fmt.Println(templates.Tree)
 	http.HandleFunc("/", home)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
