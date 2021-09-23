cb9ff24bb3c70f2e78de7558e7408e48b8b04e39
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-09-23 23:48:13 +0900

    Updated JSON Decode

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/NoTabaco/nomadcoin/blockchain"
+	"github.com/NoTabaco/nomadcoin/utils"
 )
 
 const port string = ":4000"
@@ -24,6 +27,10 @@ type URLDescription struct {
 	Payload     string `json:"payload,omitempty"`
 }
 
+type AddBlockBody struct {
+	Message string
+}
+
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
 		{
@@ -33,9 +40,19 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 		},
 		{
 			URL:         URL("/blocks"),
+			Method:      "GET",
+			Description: "See All Blocks",
+		},
+		{
+			URL:         URL("/blocks"),
 			Method:      "POST",
 			Description: "Add a Block",
 			Payload:     "data:string",
+		},
+		{
+			URL:         URL("/blocks/{id}"),
+			Method:      "GET",
+			Description: "See a Block",
 		},
 	}
 	rw.Header().Add("Content-Type", "application/json")
@@ -50,8 +67,26 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(rw).Encode(data)
 }
 
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		{
+			rw.Header().Add("Content-Type", "application/json")
+			json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+		}
+	case "POST":
+		{
+			var addBlockBody AddBlockBody
+			utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+			blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+			rw.WriteHeader(http.StatusCreated)
+		}
+	}
+}
+
 func main() {
 	http.HandleFunc("/", documentation)
+	http.HandleFunc("/blocks", blocks)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
