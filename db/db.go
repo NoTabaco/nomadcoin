0a310088dd098d245a1d5be95ab0788a08e3075c
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-10-01 04:01:05 +0900

    Creating the Database

---
+++ db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"github.com/NoTabaco/nomadcoin/utils"
+	"github.com/boltdb/bolt"
+)
+
+const (
+	dbName       = "blockchain.db"
+	dataBucket   = "data"
+	blocksBucket = "blocks"
+)
+
+var db *bolt.DB
+
+func DB() *bolt.DB {
+	if db == nil {
+		dbPointer, err := bolt.Open(dbName, 0600, nil)
+		utils.HandleErr(err)
+		db = dbPointer
+		db.Update(func(t *bolt.Tx) error {
+			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
+			utils.HandleErr(err)
+			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
+			utils.HandleErr(err)
+			return nil
+		})
+	}
+	return db
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/NoTabaco/nomadcoin
 go 1.17
 
 require github.com/gorilla/mux v1.8.0
+
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	golang.org/x/sys v0.0.0-20210930141918-969570ce7c6c // indirect
+)
