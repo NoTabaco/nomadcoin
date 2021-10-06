ef51b36944e9943a2244534d13dd65d15d29a01b
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-10-06 21:09:26 +0900

    PoW Proof of Concept

--- blockchain/block.go
+++ blockchain/block.go
@@ -9,11 +9,15 @@ import (
 	"github.com/NoTabaco/nomadcoin/utils"
 )
 
+const difficulty int = 2
+
 type Block struct {
-	Height   int    `json:"height"`
-	Data     string `json:"data"`
-	Hash     string `json:"hash"`
-	PrevHash string `json:"prevHash,omitempty"`
+	Height     int    `json:"height"`
+	Data       string `json:"data"`
+	Hash       string `json:"hash"`
+	PrevHash   string `json:"prevHash,omitempty"`
+	Difficulty int    `json:"difficulty"`
+	Nonce      int    `json:"nonce"`
 }
 
 func (b *Block) persist() {
--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	"github.com/NoTabaco/nomadcoin/cli"
-	"github.com/NoTabaco/nomadcoin/db"
+	"crypto/sha256"
+	"fmt"
+	"strings"
 )
 
 func main() {
-	cli.Start()
-	defer db.Close()
+	difficulty := 6
+	target := strings.Repeat("0", difficulty)
+	nonce := 1
+	for {
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello"+fmt.Sprint(nonce))))
+		fmt.Printf("Hash: %s\nTarget: %s\nNonce: %d\n\n", hash, target, nonce)
+		if strings.HasPrefix(hash, target) {
+			return
+		} else {
+			nonce++
+		}
+	}
 }
